195db6a525acf22bd042fde4b8d238370a1a363d
IrGringo <ringovic19@gmail.com>
2025-08-29 18:37:27 +0300

    Update about_me.py

--- views/about_me.py
+++ views/about_me.py
@@ -4,7 +4,7 @@ import streamlit as st
 #--- HERO SECTION---
 col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
 with col1:
-    st.image("assets/profile.jpg", width=230)
+    st.logo("assets/profile.jpg", width=230)
 
 with col2:
     st.title("Amidou Nahimana", anchor=False)
